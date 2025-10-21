e477ddd860399b789a34547935a8dd710636dc2e
Baraa <engbaraakhamaysa@gmail.com>
2025-10-21 09:04:43 +0300

    Add Regiester API

---
+++ src/Api/Api.js
@@ -0,0 +1,2 @@
+export const baseURL = "http://localhost:8000/api";
+export const REGISTER = "auth/register";
--- src/Pages/Auth/Register.js
+++ src/Pages/Auth/Register.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { baseURL, REGISTER } from "../../Api/Api";
+import axios from "axios";
 
 export default function Register() {
   //Obj Setates
@@ -7,14 +9,26 @@ export default function Register() {
     email: "",
     password: "",
   });
-  //Handel Form Change insed Form
+  //Handel Form Change
   function handleChange(e) {
     setForm({ ...form, [e.target.name]: e.target.value });
   }
-  console.log(form);
+  //Handle Submit
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log("run");
+
+    try {
+      console.log(`${baseURL}/${REGISTER}`);
+      await axios.post(`${baseURL}/${REGISTER}`, form);
+      console.log("Succesflly");
+    } catch (err) {
+      console.error(err);
+    }
+  }
   return (
     <div className="container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="name">Name:</label>
           <input
