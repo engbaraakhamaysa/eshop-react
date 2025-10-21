1b52aa5e4e98847ad751dbd45d4e19fbae7df121
Baraa <engbaraakhamaysa@gmail.com>
2025-10-21 09:14:21 +0300

    Add Login Page

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,2 +1,3 @@
 export const baseURL = "http://localhost:8000/api";
 export const REGISTER = "auth/register";
+export const LOGIN = "auth/Login";
--- src/Pages/Auth/Login.js
+++ src/Pages/Auth/Login.js
@@ -1,3 +1,57 @@
+import { useState } from "react";
+import { baseURL, LOGIN } from "../../Api/Api";
+import axios from "axios";
+
 export default function Login() {
-  return <h1>Login Page</h1>;
+  //Obj Setates
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  //Handel Form Change
+  function handleChange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+  //Handle Submit
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log("run");
+
+    try {
+      console.log(`${baseURL}/${LOGIN}`);
+      await axios.post(`${baseURL}/${LOGIN}`, form);
+      console.log("Succesflly");
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="email">Email:</label>
+          <input
+            name="email"
+            id="email"
+            value={form.email}
+            type="email"
+            placeholder="Enter Your Email..."
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password">Password:</label>
+          <input
+            name="password"
+            id="password"
+            value={form.password}
+            type="password"
+            placeholder="Enter Your Password..."
+            onChange={handleChange}
+          />
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
